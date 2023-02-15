1dbef0c9c899d22887b02bd72b9727e9b654cd6f
Morten Jessen <mortenjessen5d@gmail.com>
2023-02-15 22:33:47 +0100

    feat: add operation field to packet

--- crates/protocol/src/packet.rs
+++ crates/protocol/src/packet.rs
@@ -4,7 +4,8 @@ use uuid::Uuid;
 #[derive(Debug)]
 pub struct Packet<'a> {
     connection_id: Uuid,
-    topic: u8,
+    device: u8,
+    operation: u8,
     data: &'a [u8],
 }
 
@@ -14,16 +15,22 @@ impl Packet<'_> {
             return Err(anyhow!("connection id cannot be nil"));
         }
 
-        let topic = match buf.first() {
-            Some(&topic) => topic,
-            None => return Err(anyhow!("failed to parse topic")),
+        let device = match buf.first() {
+            Some(&device) => device,
+            None => return Err(anyhow!("failed to parse device")),
         };
 
-        let data = &buf[1..];
+        let operation = match buf.get(1) {
+            Some(&operation) => operation,
+            None => return Err(anyhow!("failed to parse operation")),
+        };
+
+        let data = &buf[2..];
 
         Ok(Packet {
             connection_id,
-            topic,
+            device,
+            operation,
             data,
         })
     }
@@ -50,9 +57,21 @@ mod tests {
     }
 
     #[test]
-    fn unparsed_topic() {
-        let expected = "failed to parse topic";
+    fn unparsed_device() {
+        let expected = "failed to parse device";
         let buf = vec![];
+
+        let actual = Packet::parse(Uuid::new_v4(), &buf)
+            .expect_err("expected err")
+            .to_string();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn unparsed_operation() {
+        let expected = "failed to parse operation";
+        let buf = vec![53];
 
         let actual = Packet::parse(Uuid::new_v4(), &buf)
             .expect_err("expected err")
@@ -64,28 +83,32 @@ mod tests {
     #[test]
     fn success_with_data() {
         let expected_uuid = Uuid::new_v4();
-        let expected_topic = 42;
+        let expected_device = 42;
+        let expected_operation = 55;
         let expected_data = vec![1, 2, 3];
-        let buf = vec![42, 1, 2, 3];
+        let buf = vec![42, 55, 1, 2, 3];
 
         let packet = Packet::parse(expected_uuid, &buf).expect("failed to parse packet");
 
         assert_eq!(expected_uuid, packet.connection_id);
-        assert_eq!(expected_topic, packet.topic);
+        assert_eq!(expected_device, packet.device);
+        assert_eq!(expected_operation, packet.operation);
         assert_eq!(expected_data, packet.data);
     }
 
     #[test]
     fn success_without_data() {
         let expected_uuid = Uuid::new_v4();
-        let expected_topic = 42;
+        let expected_device = 42;
+        let expected_operation = 66;
         let expected_data: Vec<u8> = vec![];
-        let buf = vec![42];
+        let buf = vec![42, 66];
 
         let packet = Packet::parse(expected_uuid, &buf).expect("failed to parse packet");
 
         assert_eq!(expected_uuid, packet.connection_id);
-        assert_eq!(expected_topic, packet.topic);
+        assert_eq!(expected_device, packet.device);
+        assert_eq!(expected_operation, packet.operation);
         assert_eq!(expected_data, packet.data);
     }
 }
